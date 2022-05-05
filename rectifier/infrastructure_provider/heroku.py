1a903c5122bf3254268fe0e8155fd757e536d24d
Alexandru Kis <alex@sectorlabs.ro>
2022-05-05 21:42:05 +0300

    Log the API key that failed to make Heroku requests

--- rectifier/infrastructure_provider/heroku.py
+++ rectifier/infrastructure_provider/heroku.py
@@ -1,4 +1,5 @@
-from typing import Dict
+from typing import Dict, Tuple
+import re
 
 import structlog
 import heroku3
@@ -11,9 +12,9 @@ from rectifier.infrastructure_provider import (
     InfrastructureProvider,
     InfrastructureProviderError,
 )
-
 from rectifier import settings
 from rectifier.get_random_key import get_random_key
+from rectifier.obfuscate_string import obfuscate_string
 
 LOGGER = structlog.get_logger(__name__)
 
@@ -36,15 +37,15 @@ class Heroku(InfrastructureProvider):
             return
 
         try:
-            app = self._connection(app_name)
+            app, key = self._connection(app_name)
             app.batch_scale_formation_processes(scale_requests)
         except (HTTPError, ResponseError) as e:
             message = 'Failed to scale.'
-            LOGGER.error(message, app=app_name, error=e)
+            LOGGER.error(message, app=app_name, error=e, key=obfuscate_string(key))
             raise InfrastructureProviderError(message)
         except RateLimitExceeded as e:
             message = 'Rate limit exceeded.'
-            LOGGER.error(message, app=app_name, error=e)
+            LOGGER.error(message, app=app_name, error=e, key=obfuscate_string(key))
             raise InfrastructureProviderError(message)
 
     def broker_uri(self, app_name: str):
@@ -53,27 +54,28 @@ class Heroku(InfrastructureProvider):
         """
 
         try:
-            app = self._connection(app_name)
+            app, key = self._connection(app_name)
             return app.config()[settings.BROKER_URL_KEY]
         except HTTPError as e:
             message = 'Cannot retrieve the broker uri.'
-            LOGGER.error(message, app=app_name, error=e)
+            LOGGER.error(message, app=app_name, error=e, key=obfuscate_string(key))
             raise InfrastructureProviderError(message)
         except RateLimitExceeded as e:
             message = 'Rate limit exceeded.'
-            LOGGER.error(message, app=app_name, error=e)
+            LOGGER.error(message, app=app_name, error=e, key=obfuscate_string(key))
             raise InfrastructureProviderError(message)
 
     @staticmethod
-    def _connection(app_name: str) -> HerokuApp:
+    def _connection(app_name: str) -> Tuple[HerokuApp, str]:
         """Gets a connection to Heroku."""
         key = get_random_key(settings.HEROKU_API_KEYS)
+
         if not key:
             message = 'No API key set'
             LOGGER.error('No API key set')
             raise InfrastructureProviderError(message)
 
-        conn = heroku3.from_key(get_random_key(settings.HEROKU_API_KEYS))
+        conn = heroku3.from_key(key)
         apps = conn.apps()
 
         if app_name not in apps:
@@ -81,4 +83,4 @@ class Heroku(InfrastructureProvider):
             LOGGER.error(message, app=app_name, apps=apps)
             raise InfrastructureProviderError(message)
 
-        return apps[app_name]
+        return apps[app_name], key
